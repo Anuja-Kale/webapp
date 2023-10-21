bc3ac6c783ac8fa9b3cf8becc9720a2a21d792f9
anujakale14 <62926535+Anuja-Kale@users.noreply.github.com>
2023-10-21 14:54:41 -0400

    webapp code

--- Models/db.js
+++ Models/db.js
@@ -1,10 +1,22 @@
-const Sequelize = require('sequelize'); 
-require('dotenv').config();
+// const Sequelize = require('sequelize'); 
+// require('dotenv').config();
 
-const sequelize = new Sequelize(process.env.DB_NAME,process.env.DB_USER,process.env.DB_PASSWORD,{
-    dialect: 'mysql',
-    host:'localhost',
-    port: 3306
-});
+// const sequelize = new Sequelize(process.env.DB_NAME,process.env.DB_USER,process.env.DB_PASSWORD,{
+//     dialect: 'mysql',
+//     host:'localhost',
+//     port: 3306
+// });
 
+// module.exports = sequelize
+
+const sequelize = new Sequelize(
+    process.env.DB_DATABASE, 
+    process.env.DB_USERNAME, 
+    process.env.DB_PASSWORD, 
+    {
+        host: process.env.DB_HOST,
+        dialect: 'mysql',
+        port: 3306,
+    }
+);
 module.exports = sequelize
