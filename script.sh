291e38056eb1ebfcde06fdb1975975ccaa77eb2e
anujakale14 <62926535+Anuja-Kale@users.noreply.github.com>
2023-10-21 14:59:43 -0400

    webapp code

--- script.sh
+++ script.sh
@@ -1,37 +1,14 @@
 #!/bin/bash
 
-# Load environment variables from .env file
-# source .env
-
-# # Securely read the MySQL root password from an environment variable
-# if [ -z "$MYSQL_ROOT_PASSWORD" ]; then
-#   echo "MySQL root password not set. Exiting."
-#   exit 1
-# fi
-
-# # Set MySQL root password
-# mysql --user=root --password="$MYSQL_ROOT_PASSWORD" -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD';"
-
-# # Additional MySQL configuration commands
-# # For example, create a database and user
-# # mysql --user=root --password="$MYSQL_ROOT_PASSWORD" -e "CREATE DATABASE mydb;"
-# # mysql --user=root --password="$MYSQL_ROOT_PASSWORD" -e "CREATE USER 'myuser'@'localhost' IDENTIFIED BY 'mypassword';"
-# # mysql --user=root --password="$MYSQL_ROOT_PASSWORD" -e "GRANT ALL PRIVILEGES ON mydb.* TO 'myuser'@'localhost';"
-# # ...
-
-# # Restart MySQL service
-# sudo systemctl restart mysql
-
-
-
-#!/bin/bash
-
 # Update package repositories
 sudo apt-get update
 
 # Install Node.js and npm
 curl -fsSL https://deb.nodesource.com/setup_16.x | sudo -E bash -
 sudo apt-get install -y nodejs
+
+# Install sequelize globally
+npm install -g sequelize
 
 # Install MariaDB
 sudo debconf-set-selections <<< 'mariadb-server-10.5 mysql-server/root_password password root1234'
@@ -46,9 +23,6 @@ sudo systemctl start mysql
 # Replace with your specific database setup commands
 # Example:
 # mysql -u root -p"your-root-password" -e "CREATE DATABASE webappdb;"
-
-# Optionally, you can include additional application-specific setup steps here.
-
 
 # Enable MariaDB to start on boot
 sudo systemctl enable mysql
