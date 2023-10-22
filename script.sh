05daed396244cb0fc909031b23445c1d9d0943d3
anujakale14 <62926535+Anuja-Kale@users.noreply.github.com>
2023-10-22 11:26:20 -0400

    webapp code

--- script.sh
+++ script.sh
@@ -5,14 +5,15 @@ sudo apt update && sudo apt -y upgrade
 
 # Install Node.js and npm from the nodesource repository for the latest version
 curl -fsSL https://deb.nodesource.com/setup_16.x | sudo -E bash -
-sudo apt-get install -y nodejs npm
+sudo apt-get install -y nodejs
 
-# Install sequelize globally
+# The 'npm' package isn't required as the latest Node.js comes with npm. If it doesn't, remove the comment from the line below.
+# sudo apt-get install -y npm
+
+# Install sequelize globally (if needed in your case)
 npm install -g sequelize
 
-# Install MariaDB
-sudo debconf-set-selections <<< 'mariadb-server-10.5 mysql-server/root_password password root'
-sudo debconf-set-selections <<< 'mariadb-server-10.5 mysql-server/root_password_again password root'
+# Install MariaDB server and client
 sudo apt-get install -y mariadb-server mariadb-client
 
 # Start and enable MariaDB
@@ -26,7 +27,7 @@ if ! sudo systemctl is-active --quiet mariadb; then
 fi
 
 # Create a database if it doesn't exist
-DB_NAME="UserDataBase"
+DB_NAME="projectDatabase"  # Updated to the new database name
 
 if sudo mysql -u root -e "USE $DB_NAME" 2>/dev/null; then
     echo "Database $DB_NAME already exists."
@@ -42,21 +43,20 @@ SQL
 fi
 
 # Secure MariaDB installation (set root password and remove anonymous users)
+# Since your password is already 'root', we'll make sure it remains unchanged during the secure installation.
 sudo mysql_secure_installation <<EOF
 
+n
 root
-n
-n
+root
 y
 y
 y
 y
 EOF
 
-# Ensure /opt/webapp directory exists
+# Ensure /opt/webapp directory exists and has the right permissions
 sudo mkdir -p /opt/webapp
-
-# Adjust permissions for the webapp directory
 sudo chown -R $(whoami) /opt/webapp
 
 # Change to webapp directory and install sequelize and mysql using npm
@@ -78,6 +78,13 @@ User=nobody
 
 [Install]
 WantedBy=multi-user.target" | sudo tee /etc/systemd/system/webapp.service
+
+# Reload systemd to recognize new service
+sudo systemctl daemon-reload
+
+# Enable and start the new service
+sudo systemctl enable webapp.service
+sudo systemctl start webapp.service
 
 # Clean up (remove unnecessary packages and clear cache)
 sudo apt-get autoremove -y
