a91aae9ed88b82ca93e7d6ae70910658f395cfc1
anujakale14 <62926535+Anuja-Kale@users.noreply.github.com>
2023-10-26 20:30:16 -0400

    Update db.js

--- Models/db.js
+++ Models/db.js
@@ -36,7 +36,7 @@ if (process.env.NODE_ENV !== 'envProd') {
 // const DB_PASSWORD = "J8adestroyvQr#9zL4y";
 // const DB_HOST = "csye6225-dbfcec3f7.cnrttrsz0ctr.us-east-1.rds.amazonaws.com";
 
-const sequelize = new Sequelize(process.env.DB_USER, process.env.DB_NAME, process.env.DB_PASSWORD, {
+const sequelize = new Sequelize(process.env.DB_USERNAME, process.env.DB_DATABASE, process.env.DB_PASSWORD, {
     host: process.env.DB_HOST,
     dialect: 'mysql',
     port: 3306,
