04d15ed2bdbded77d842c7b4df2c993589ff8027
Anuja Kale <62926535+Anuja-Kale@users.noreply.github.com>
2023-11-10 16:45:27 -0500

    Cloud watch webapp (#73)
    * webapp code
    
    * webapp code
    
    * webapp code
    
    * webapp code
    
    * webapp code
    
    * Update pck.pkr.hcl
    
    * Update pck.pkr.hcl
    
    * Update pck.pkr.hcl
    
    * update
    
    * Update script.sh
    
    * updates
    
    * Update assignmentController.js
    
    * updates
    
    * update
    
    * Update server.js
    
    * Update script.sh
    
    * update
    
    * update
    
    * update
    
    * update
    
    * Update pck.pkr.hcl
    
    * updates
    
    * updates
    
    * updated
    
    * update

---
+++ Utils/StatsD_client.js
@@ -0,0 +1,7 @@
+const StatsD = require('node-statsd');
+const statsd = new StatsD({
+    host: 'localhost', // The hostname or IP of your StatsD server
+    port: 8125, // Default StatsD port
+    prefix: 'yourapp.',
+    globalTags: { environment: process.env.NODE_ENV } // Optional global tags
+});
