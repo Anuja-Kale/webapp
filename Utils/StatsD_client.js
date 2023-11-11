00e214eb07b55ba5cb076168cdcfd2d41b1b4d7c
anujakale14 <62926535+Anuja-Kale@users.noreply.github.com>
2023-11-11 16:18:56 -0500

    update

--- Utils/StatsD_client.js
+++ Utils/StatsD_client.js
@@ -1,18 +1,18 @@
-const StatsD = require('node-statsd');
-const statsd = new StatsD({
-    host: 'localhost', // The hostname or IP of your StatsD server
-    port: 8125, // Default StatsD port
-    prefix: 'yourapp.',
-    globalTags: { environment: process.env.NODE_ENV } // Optional global tags
-});
-module.exports = statsd;
-
-// const { StatsD } = require('hot-shots');
+// const StatsD = require('node-statsd');
 // const statsd = new StatsD({
-//   host: 'localhost',
-//   port: 8125,
-//   prefix: 'yourapp.',
-//   globalTags: { environment: process.env.NODE_ENV },
+//     host: 'localhost', // The hostname or IP of your StatsD server
+//     port: 8125, // Default StatsD port
+//     prefix: 'yourapp.',
+//     globalTags: { environment: process.env.NODE_ENV } // Optional global tags
 // });
-
 // module.exports = statsd;
+//test
+const { StatsD } = require('hot-shots');
+const statsd = new StatsD({
+  host: 'localhost',
+  port: 8125,
+  prefix: 'yourapp.',
+  globalTags: { environment: process.env.NODE_ENV },
+});
+
+module.exports = statsd;
